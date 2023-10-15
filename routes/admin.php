da2f95f44a4a0219fe52e95d79ad4a45e2af61a0
osa5d <osamah.bing@gmail.com>
2023-10-15 18:35:15 +0300

    test the file

--- routes/admin.php
+++ routes/admin.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Admin\FrontendController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| yes thist is me test xampp.
 */
 
 Auth::routes();
